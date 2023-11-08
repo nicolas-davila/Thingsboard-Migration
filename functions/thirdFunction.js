4e7f1148012cd83ecf6271a7d6a1841845f3be71
Nicolas Dávila Ferreira Santos <123404361+nicolas-davila@users.noreply.github.com>
2023-11-08 16:35:29 -0300

    Update thirdFunction.js

--- functions/thirdFunction.js
+++ functions/thirdFunction.js
@@ -1,6 +1,6 @@
 let token = msg.payload.token;
 let uarms = flow.get("uarms");
-let matriz = [];
+let array = [];
 
 msg.headers = {
     //'X-Authorization': "Bearer$eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZWluYWxkb0BpZXRlY25vbG9naWEuY29tIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiI2ZmU4MjQxMC1mMmFkLTExZWEtOTY5Zi05NWRiNWUyMWM1YTIiLCJmaXJzdE5hbWUiOiJSZWluYWxkbyIsImxhc3ROYW1lIjoiQWJyZXUiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiNjUyY2YyZDAtZjJhZC0xMWVhLTk2OWYtOTVkYjVlMjFjNWEyIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNjQ2OTQxMjA0LCJleHAiOjE2NDY5NTAyMDR9.2MoQxnse6QHPOsOC14wXoWSR6t4jyjZrwVThuQGF2RNFBjizrF2E5jINakTugvQiu9n0nDRs-cO7BrNgMUM0qw"
@@ -18,12 +18,12 @@ for(i = 0; i < uarms.length; i++){
             uarms: uarms[i].value,
         }
     } 
-    matriz.push(object);
+    array.push(object);
 }
 
 // const jsonString = JSON.stringify(matriz);
 // const object = JSON.parse(jsonString);
 
-msg.payload = matriz;
+msg.payload = array;
 
 return msg;
